9c125a86fcdf499f012dd42dc425c04487bdf499
NuelUzoma <emcool17@gmail.com>
2022-09-09 23:32:24 +0000

    Fixing error on puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +6,7 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+        puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
