0d657cdb62c00d006f4962b95199491b97505db1
NuelUzoma <emcool17@gmail.com>
2022-09-11 01:31:55 +0000

    Possible Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * possible Combinations of single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
