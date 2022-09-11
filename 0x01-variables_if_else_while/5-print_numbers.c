a4a2c50cdb8df3aa3e9d94a63976116284686ff3
NuelUzoma <emcool17@gmail.com>
2022-09-11 00:46:27 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Prints all single digits into base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+for (digit = 0; digit < 10; digit++)
+{
+printf("%d", digit);
+printf("\n");
+}
+return (0);
+}
