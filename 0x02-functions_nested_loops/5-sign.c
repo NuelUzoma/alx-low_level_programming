e537288840361d73dd804dc6efabc6af3d03d057
NuelUzoma <emcool17@gmail.com>
2022-09-13 18:40:11 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign-prints the sign of a number
+ * @n: is the variable of this program
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 48)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
