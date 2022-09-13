097ae8c560e9d2eb51d63d033f052e398e093d9b
NuelUzoma <emcool17@gmail.com>
2022-09-13 15:04:48 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-Entry Point
+ * A Basic Program to print _putchar function
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This a a Repository for all my projects on Functions and Nested Loops in C Programming
