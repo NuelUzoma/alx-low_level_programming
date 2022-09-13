29c12b119a1bcd45711b22541ffe9a51ad97fbb7
NuelUzoma <emcool17@gmail.com>
2022-09-13 22:36:17 +0000

    abs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,20 @@
  */
 int print_sign(int n)
 {
-	if (n > 48)
+
+	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 48)
+	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
-	else if (n < 48)
+	else
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
 	}
 
