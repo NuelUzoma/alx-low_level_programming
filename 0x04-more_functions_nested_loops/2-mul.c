018062adbdd69d0c3cd07ebc7b7794f76faca2a5
NuelUzoma <emcool17@gmail.com>
2022-09-15 21:43:31 +0000

    Mulply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - takes the variables given and
+ * multiplies them
+ * @a: integer
+ * @b: integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
