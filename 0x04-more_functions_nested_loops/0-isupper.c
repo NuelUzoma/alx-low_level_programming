921da3eb5d496f3ff35f16796e97bd45a5ce3398
NuelUzoma <emcool17@gmail.com>
2022-09-15 16:44:56 +0000

    Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,15 @@
  */
 int _isupper(int c)
 {
-	if (c <= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 	else
 	{
-		return (0);
+		if (c >= 97 && c <= 122)
+		{
+			return (0);
+		}
 	}
 }
