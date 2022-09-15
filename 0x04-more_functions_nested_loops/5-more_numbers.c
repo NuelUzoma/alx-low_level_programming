592e7689d179f8c785ca4c3feb2e4d0c0c90871b
NuelUzoma <emcool17@gmail.com>
2022-09-15 22:17:08 +0000

    10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print ten times
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
