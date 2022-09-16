5e64417d4e8be4ba2ea19da48a1866348d393bd2
NuelUzoma <emcool17@gmail.com>
2022-09-16 05:08:25 +0000

    abeg

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,17 @@ void print_triangle(int size)
 	int a, b, c, d;
 
 	d = 1;
+	e = size;
 	if (size <= 0)
 	_putchar('\n');
 	for (b = 0; b < size; b++)
 	{
-	for (a = 0; a < (size - 1); a++)
+	for (a = 0; a < (e - 1); a++)
 	_putchar(' ');
 	for (c = 0; c < d; c++)
 	_putchar('#');
 	_putchar('\n');
-	size--;
+	e--;
 	d++;
 	}
 }
