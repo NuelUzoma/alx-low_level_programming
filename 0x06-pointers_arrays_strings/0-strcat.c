a842ce6f2d0bc54bdfad5b739defbd90920f7505
NuelUzoma <emcool17@gmail.com>
2022-09-22 14:05:18 +0000

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings
+ * dest - pointer variable
+ * src - pointer variable
+ * Return: 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	printf("%s %s", dest, src);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repository contains the part 2 of the last project on pointers,arrays and strings with ALX and Holberton.
