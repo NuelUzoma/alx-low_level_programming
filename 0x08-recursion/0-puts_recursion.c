f36f220c0065b4fcee5effe3ae44d56e1cca0519
NuelUzoma <emcool17@gmail.com>
2022-09-28 14:50:15 +0000

    puts function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - a function that prints a string
+ * followed by a new line
+ * @s: char variable
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This Repository is all about recursion, implementation of recursion, situations we should and shouldnt use recursion in our C programs.
