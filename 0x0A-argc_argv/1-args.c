d5485cc9d2e4946be59b228b7ff3dc2e4b4eb0a3
NuelUzoma <emcool17@gmail.com>
2022-09-30 18:02:55 +0000

    unused

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-#include "unused.h"
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define(UNUSED(x) UNUSED_ ## x __attribute__((unused)))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
 
+
+void dcc_mon_siginfo_handler(int UNUSED(whatsig));
+
+#endif
 /**
  * main - a program that prints the number
  * of arguments passed to it
