b52a36229cc28c6042feaf55a97193ff9f7d7a41
NuelUzoma <emcool17@gmail.com>
2022-10-10 20:43:09 +0100

    File

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function of the program
+ * Description: Short Description
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
