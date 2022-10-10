0c4410430754c9f9d9eb63112841e91557457523
NuelUzoma <emcool17@gmail.com>
2022-10-10 17:43:49 +0100

    dog initials

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This drectory contains my projects on structres and typedef and how to nest data types and also how to utilise the struct function with pointers.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data types for the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ *
+ * Description: Short Description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
