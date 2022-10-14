cd51f01082edd236e0ecaaa3c76b7250b26f8e19
NuelUzoma <emcool17@gmail.com>
2022-10-14 19:01:21 +0100

    Both at once

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,8 +5,20 @@
  * @array: array of integers
  * @size: size of array
  * @cmp: function pointer
+ * Return: an integer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (size <= 0 || array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char- a function that prints a character
+ * @arg: the charcter
+ * Return: Nothing(void)
+ */
+void print_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
+}
+
+/**
+ * print_int- A function that prints integer
+ * @arg: The integer
+ * Returns: Nothing(void)
+ */
+void print_int(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%i", i);
+}
+
+/**
+ * print_float- A function that prints a real number
+ * @arg: the float number
+ * Return: Nothing(void)
+ */
+void print_float(va_list arg)
+{
+	float f;
+
+	f = va_arg(arg, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string- A function that prints a string
+ * @arg:The string
+ * Return: Nothing(void)
+ */
+void print_string(va_list arg)
+{
+	char *s;
+
+	s = va_arg(arg, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", s);
+}
+/**
+ * print_all- A function that prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
