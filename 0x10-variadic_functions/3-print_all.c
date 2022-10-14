a81723e23d597faaa8e25f02c1e3a02e75be5056
NuelUzoma <emcool17@gmail.com>
2022-10-14 19:23:23 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
  * print_char- a function that prints a character
  * @arg: the charcter
@@ -72,24 +66,15 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
-	printer_t funcs[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
 
 	va_start(args, format);
 
 	while (format && (*(format + i)))
 	{
 		j = 0;
-		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-			j++;
 		if (j < 4)
 		{
 			printf("%s", separator);
-			funcs[j].print(args);
 			separator = ", ";
 		}
 		i++;
