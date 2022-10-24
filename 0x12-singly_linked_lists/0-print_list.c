3efe656a3aee236bba130938f6444be3159879c1
NuelUzoma <emcool17@gmail.com>
2022-10-24 17:36:22 +0100

    Linked List 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
+	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
