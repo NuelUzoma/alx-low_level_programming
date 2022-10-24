6aff79d833468f734a74a4af63b2fe787652bbcb
NuelUzoma <emcool17@gmail.com>
2022-10-24 23:03:51 +0100

    Advanced Task 1 and 2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
-#include "lists.h"
+#include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void __attribute__ ((constructor)) first(void);
 /**
  * first - A function that prints before the main executes
  */
 void first(void)
 {
-	printf("You 're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	printf("You 're beat! and yet, you must allow, \n"
+		"I bore my house upon my back!\n");
 }
