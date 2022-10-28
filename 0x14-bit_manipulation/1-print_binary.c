d00993e7686e893baf3641fc9cc17092b3e05532
NuelUzoma <emcool17@gmail.com>
2022-10-28 21:31:17 +0100

    Numbers to Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary of a number
+ * @n: unsigned integer
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+
+	printf("%ld", n % 2);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+int _strlen(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
